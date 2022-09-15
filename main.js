50de5ac97c8f0d204c04387c05a9ee8c5975ea54
sergio-nogueira <serque@gmail.com>
2022-09-14 21:47:45 -0300

    Layout update

--- main.js
+++ main.js
@@ -2,28 +2,56 @@ window.onload = init
 
 function init(){
 
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
+
+const overlay = new ol.Overlay({
+  element: container,
+  autoPan: {
+    animation: {
+      duration: 250,
+    },
+  },
+});
+
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+  const view = new ol.View({
+    center: [-50, -13.5],
+    zoom: 5.6,
+    projection: 'EPSG:4326',
+    })
+
   const map = new ol.Map({
-    view: new ol.View({
-      center: [-50, -13.5],
-      zoom: 5.2,
-      projection: 'EPSG:4326',
-      }),
+    view: view,
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM(),
         visible: true
       })
     ],
-    target: "js-map-desmatamento",
+    overlays: [overlay],
+    target: "js-map",
   })
+
+const PotCropLatFonte = new ol.source.TileWMS({
+  ratio: 1,
+  url: 'http://localhost:8080/geoserver/cerrado/wms',
+  params: {'VERSION': '1.1.1',  
+        "STYLES": '',
+        "LAYERS": 'cerrado:PotCropLat',
+        "exceptions": 'application/vnd.ogc.se_inimage',
+  }
+})
 
   // Dados espacializados por municipio
   const PotCropLat = new ol.layer.Tile({
-    source: new ol.source.TileWMS({
-      url:'http://ec2-34-201-126-65.compute-1.amazonaws.com:8080/geoserver/tese/wms?service=WMS&version=1.1.0&request=GetMap&layers=tese%3APotCropLat',
-      }),
-      visible: false,
-      title: 'PotCropLat'
+    source: PotCropLatFonte
   })
 
   const AgicCO = new ol.layer.Tile({
@@ -94,9 +122,13 @@ function init(){
     source: new ol.source.TileWMS({
       url:'http://ec2-34-201-126-65.compute-1.amazonaws.com:8080/geoserver/tese/wms?service=WMS&version=1.1.0&request=GetMap&layers=tese%3AD16a20',
       }),
-      visible: true,
+      visible: false,
       title: 'D16a20'
   })
+
+  // Legend
+
+
   
   // Layer Group
   const baseLayerGroup = new ol.layer.Group({
@@ -113,7 +145,7 @@ function init(){
       url:'http://ec2-34-201-126-65.compute-1.amazonaws.com:8080/geoserver/tese/wms?service=WMS&version=1.1.0&request=GetMap&layers=tese%3Abi_ce_uf_2021',
       })
   })
-  map.addLayer(bi_ce_uf_2021);
+  //map.addLayer(bi_ce_uf_2021);
 
   const baseLayerElements = document.querySelectorAll('.sidebar > input[type=radio]')
   for(let baseLayerElement of baseLayerElements){
@@ -142,9 +174,38 @@ function init(){
     steps: 4,
     text: false
   })
-  //map.addControl(scaleLineControl);
+  map.addControl(scaleLineControl);
 
+// Feature Info
 
-
-
+map.on('singleclick', function (evt) {
+  content.innerHTML = 'Sem dado'
+  const coordinate = evt.coordinate;
+  overlay.setPosition(coordinate);
+  const viewResolution = /** @type {number} */ (view.getResolution());
+  const url = PotCropLatFonte.getFeatureInfoUrl(
+    evt.coordinate,
+    viewResolution,
+    'EPSG:4326',
+    {'INFO_FORMAT': 'application/json',
+    'propertyName': 'NM_MUN,SIGLA,PotCropLat,AgicCO,Restaur',
+    'exclude_nodata_result': 'true'},
+  );
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(resposta) {
+    const var1 = resposta['features']
+    const var2 = var1[0]
+    const var3 = var2['properties']
+    const municipio = var3['NM_MUN']
+    const sigla = var3['SIGLA']
+    const expansao = var3['PotCropLat']
+    const custo = var3['AgicCO']
+    const restauracao = var3['Restaur']
+    content.innerHTML = `<center><b><font size=3>${municipio} - ${sigla}</b></font></center><br>
+                         <b>Expansão agrícola: </b>${expansao} ha<br>
+                         <b>Custo de oportunidade: </b>${custo}<br>
+                         <b>Restauração </b>${restauracao} ha<br>`
+   })
+});
 }
