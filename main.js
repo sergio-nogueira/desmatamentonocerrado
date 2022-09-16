dc4bc47c60c510a1af7fa3b4c0434ded8f7c7054
sergio-nogueira <serque@gmail.com>
2022-09-16 15:55:53 -0300

    layout update

--- main.js
+++ main.js
@@ -50,9 +50,14 @@ const PotCropLatFonte = new ol.source.TileWMS({
 })
 
   // Dados espacializados por municipio
+
   const PotCropLat = new ol.layer.Tile({
-    source: PotCropLatFonte
-  })
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado%3APotCropLat',
+      }),
+      visible: false,
+      title: 'PotCropLat'
+  }) 
 
   const AgicCO = new ol.layer.Tile({
     source: new ol.source.TileWMS({
@@ -118,11 +123,13 @@ const PotCropLatFonte = new ol.source.TileWMS({
       title: 'D30'
   })
 
+ 
+
   const D16a20 = new ol.layer.Tile({
     source: new ol.source.TileWMS({
       url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado%3AD16a20',
       }),
-      visible: false,
+      visible: true,
       title: 'D16a20'
   })
 
@@ -147,7 +154,7 @@ const PotCropLatFonte = new ol.source.TileWMS({
   })
   map.addLayer(bi_ce_uf_2021);
 
-  const baseLayerElements = document.querySelectorAll('.sidebar > input[type=radio]')
+  const baseLayerElements = document.querySelectorAll('.camadas > input[type=radio]')
   for(let baseLayerElement of baseLayerElements){
     baseLayerElement.addEventListener('change', function(){
       let baseLayerElementValue = this.value;
@@ -179,16 +186,16 @@ const PotCropLatFonte = new ol.source.TileWMS({
 // Feature Info
 
 map.on('singleclick', function (evt) {
-  content.innerHTML = 'Sem dado'
+  //content.innerHTML = 'Sem dado'
   const coordinate = evt.coordinate;
-  overlay.setPosition(coordinate);
+  //overlay.setPosition(coordinate);
   const viewResolution = /** @type {number} */ (view.getResolution());
   const url = PotCropLatFonte.getFeatureInfoUrl(
     evt.coordinate,
     viewResolution,
     'EPSG:4326',
     {'INFO_FORMAT': 'application/json',
-    'propertyName': 'NM_MUN,SIGLA,PotCropLat,AgicCO,Restaur',
+    'propertyName': 'NM_MUN,SIGLA,Restaur,PotCropLat,D16a20,D30,DSC,DVN,EDB,BIO,RPQAAD,AgicCO',
     'exclude_nodata_result': 'true'},
   );
   fetch(url)
@@ -199,13 +206,27 @@ map.on('singleclick', function (evt) {
     const var3 = var2['properties']
     const municipio = var3['NM_MUN']
     const sigla = var3['SIGLA']
+    const areadesmatada = var3['D16a20']
+    const estareadesmatada = var3['D30']
+    const risco = var3['RPQAAD']
+    const diassecosconsecutivos = var3['DSC']
+    const declividadevegnat = var3['DVN']
+    const erodibilidade = var3['EDB']
+    const biodiversidade = var3['BIO']
     const expansao = var3['PotCropLat']
     const custo = var3['AgicCO']
     const restauracao = var3['Restaur']
-    content.innerHTML = `<center><b><font size=3>${municipio} - ${sigla}</b></font></center><br>
-                         <b>Expansão agrícola: </b>${expansao} ha<br>
+
+    detalhes.innerHTML = `<center><b><font size=4>${municipio} - ${sigla}</b></font></center><br>
+                         <b>Área desmatada (2016-2020): </b>${areadesmatada} km²<br>
+                         <b>Estimativa de área desmatada (2020-2030): </b>${estareadesmatada} km²<br>                              
+                         <b>RPQAAD: </b>${risco} <br>
+                         <b>Dias secos consecutivos: </b>${diassecosconsecutivos} dias<br>                 
+                         <b>Declividade em vegetação nativa: </b>${declividadevegnat}%<br>
+                         <b>Erodibilidade: </b>${erodibilidade}<br>  
+                         <b>Biodiversidade: </b>${biodiversidade}<br>  
                          <b>Custo de oportunidade: </b>${custo}<br>
-                         <b>Restauração </b>${restauracao} ha<br>`
+                         <b>Restauração: </b>${restauracao} ha<br>`
    })
 });
 }
