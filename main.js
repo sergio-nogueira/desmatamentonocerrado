136fb5b928c4f8b13d4d3c811c279c6aec2f81bb
sergio-nogueira <serque@gmail.com>
2022-09-26 22:11:03 -0300

    Update layers

--- main.js
+++ main.js
@@ -21,23 +21,22 @@ closer.onclick = function () {
   return false;
 };
 
-  const view = new ol.View({
-    center: [-50, -13.5],
-    zoom: 5.6,
-    projection: 'EPSG:4326',
-    })
-
-  const map = new ol.Map({
-    view: view,
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-        visible: true
-      })
-    ],
-    overlays: [overlay],
-    target: "js-map",
+const view = new ol.View({
+  center: [-50, -13.5],
+  zoom: 5.8,
+  projection: 'EPSG:4326',
   })
+const map = new ol.Map({
+  view: view,
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+      visible: true
+    })
+  ],
+  overlays: [overlay],
+  target: "js-map",
+})
 
 const PotCropLatFonte = new ol.source.TileWMS({
   ratio: 1,
@@ -122,7 +121,6 @@ const PotCropLatFonte = new ol.source.TileWMS({
       visible: false,
       title: 'D30'
   })
-
  
 
   const D16a20 = new ol.layer.Tile({
@@ -133,6 +131,72 @@ const PotCropLatFonte = new ol.source.TileWMS({
       title: 'D16a20'
   })
 
+  const D2020 = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:d2020',
+      }),
+      visible: false,
+      title: 'd2020'
+  })
+
+  const DSCpix = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:ce_dsca_gpm_2001_2019_median_100m',
+      }),
+      visible: false,
+      title: 'ce_dsca_gpm_2001_2019_median_100m'
+  })
+
+  const DECpix = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:declividade',
+      }),
+      visible: false,
+      title: 'declividade'
+  })
+
+  const RPQAADpix = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:RPQAADpix',
+      }),
+      visible: false,
+      title: 'RPQAADpix'
+  })
+
+  const pastagem_prioridade = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:pastagem_prioridade',
+      }),
+      visible: false,
+      title: 'pastagem_prioridade'
+  })
+
+  const custo_de_oportunidade = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:custo_de_oportunidade',
+      }),
+      visible: false,
+      title: 'custo_de_oportunidade'
+  })
+
+  const bi_ce_areas_prioritarias = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:bi_ce_areas_prioritarias',
+      }),
+      visible: false,
+      title: 'bi_ce_areas_prioritarias'
+  })
+
+
+  const bi_ce_erodibilidade = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:bi_ce_erodibilidade',
+      }),
+      visible: false,
+      title: 'bi_ce_erodibilidade'
+  })
+
+
   // Legend
 
 
@@ -140,7 +204,7 @@ const PotCropLatFonte = new ol.source.TileWMS({
   // Layer Group
   const baseLayerGroup = new ol.layer.Group({
     layers: [
-      PotCropLat, AgicCO, Restaur, RPQAAD,
+      bi_ce_areas_prioritarias, bi_ce_erodibilidade, custo_de_oportunidade, pastagem_prioridade, D2020, RPQAADpix,DSCpix, DECpix, PotCropLat, AgicCO, Restaur, RPQAAD,
       BIO, DSC, DVN, EDB, D30, D16a20
     ]
   })
@@ -154,16 +218,28 @@ const PotCropLatFonte = new ol.source.TileWMS({
   })
   map.addLayer(bi_ce_uf_2021);
 
+  // Municipios
+  const municipios = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:'http://localhost:8080/geoserver/cerrado/wms?service=WMS&version=1.1.0&request=GetMap&layers=cerrado:municipios',
+      })
+  })
+  map.addLayer(municipios);
+
+
   const baseLayerElements = document.querySelectorAll('.camadas > input[type=radio]')
   for(let baseLayerElement of baseLayerElements){
     baseLayerElement.addEventListener('change', function(){
       let baseLayerElementValue = this.value;
-      baseLayerGroup.getLayers().forEach(function(element, index, array){
+      legenda.innerHTML = `<p align='left'><b><font size=4>Legenda</b></font></center></p>
+      <img src='http://localhost:8080/geoserver/cerrado/wms?Service=WMS&REQUEST=GetLegendGraphic&VERSION=1.1.0&FORMAT=image/png&WIDTH=25&HEIGHT=20&LAYER=cerrado:${baseLayerElementValue}'></img>`
+       baseLayerGroup.getLayers().forEach(function(element, index, array){
         let baseLayerName = element.get('title');
-        element.setVisible(baseLayerName === baseLayerElementValue)
+        element.setVisible(baseLayerName === baseLayerElementValue)          
       })
     })
   }
+  
 
     // TileDebug
     const tileDebugLayer = new ol.layer.Tile({
@@ -186,9 +262,7 @@ const PotCropLatFonte = new ol.source.TileWMS({
 // Feature Info
 
 map.on('singleclick', function (evt) {
-  //content.innerHTML = 'Sem dado'
   const coordinate = evt.coordinate;
-  //overlay.setPosition(coordinate);
   const viewResolution = /** @type {number} */ (view.getResolution());
   const url = PotCropLatFonte.getFeatureInfoUrl(
     evt.coordinate,
@@ -217,7 +291,7 @@ map.on('singleclick', function (evt) {
     const custo = var3['AgicCO']
     const restauracao = var3['Restaur']
 
-    detalhes.innerHTML = `<center><b><font size=4>${municipio} - ${sigla}</b></font></center><br>
+    dados.innerHTML = `<p align='left'><b><font size=4>${municipio} - ${sigla}</b></font></center><br></p>
                          <b>Área desmatada (2016-2020): </b>${areadesmatada} km²<br>
                          <b>Estimativa de área desmatada (2020-2030): </b>${estareadesmatada} km²<br>                              
                          <b>RPQAAD: </b>${risco} <br>
@@ -229,4 +303,36 @@ map.on('singleclick', function (evt) {
                          <b>Restauração: </b>${restauracao} ha<br>`
    })
 });
+
+
+const status = document.getElementById('status');
+
+
+map.on('pointermove', function (e) {
+  const coordinate = e.coordinate;
+  const viewResolution = /** @type {number} */ (view.getResolution());
+    const url = PotCropLatFonte.getFeatureInfoUrl(
+    e.coordinate,
+    viewResolution,
+    'EPSG:4326',
+    {'INFO_FORMAT': 'application/json',
+    'propertyName': 'NM_MUN,SIGLA',
+    'exclude_nodata_result': 'true'},
+  );  
+  
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(resposta) {
+    const var1 = resposta['features']
+    const var2 = var1[0]
+    const var3 = var2['properties']
+    const municipio = var3['NM_MUN']
+    const sigla = var3['SIGLA']    
+    content.innerHTML = `<font size="3"><p align="Left"><b>Municipio:</b> ${municipio}<br> <b>UF:</b> ${sigla}</p></font>`;
+    overlay.setPosition(coordinate);
+   })
+  
+});
+
 }
+
